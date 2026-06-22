98e5032311729b1bd68a49a0b633b48520438d4a
natieledpaula <natiele887@gmail.com>
2026-06-22 20:14:35 -0300

    Implemente um código onde tinha os métodos de Login, criação e Consulta de Usuários.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
+// Implemente um código onde tinha os métodos de Login, criação e Consulta de Usuários.
+// Ele deve conter o método de autenticação
+// deverá realizar a liberação do CORS
+// Deverá ter a documentação de todos os end-points existentes no sistema
+// Deverá ser implementado através de serviços
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
