c2b8b1c811e1c91031d3697adee8e797e492444f
natieledpaula <natiele887@gmail.com>
2026-06-22 20:13:33 -0300

    Controlador para gerenciar as rotas relacionadas aos usuários

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { DatabaseConfig } from './database/database.config';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
-import { User } from './user/user';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
   imports: [],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Get,
+    Param,
+    Put,
+    Delete,
+    Body,
+} from '@nestjs/common';
 
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+// Controlador para gerenciar as rotas relacionadas aos usuários
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    // Rota para criar um novo usuário
+    @Post()
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.userService.create(createUserDto);
+    }
+
+    // Rota para obter todos os usuários
+    @Get()
+    findAll() {
+        return this.userService.findAll();
+    }
+
+    // Rota para obter um usuário específico pelo ID
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(id);
+    }
+
+    // Rota para atualizar um usuário específico pelo ID
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.userService.update(id, updateUserDto);
+    }
+
+    // Rota para remover um usuário específico pelo ID
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.userService.remove(id);
+    }
+}
